22a3f077855ebd8652d3f7bf27eeac29dcc3b17a
ParisK91 <paris_k91@hotmail.com>
2023-12-27 17:08:01 +0200

    NEW MESSAGE1

--- src/gr/aueb/cf/Main.java
+++ src/gr/aueb/cf/Main.java
@@ -3,7 +3,7 @@ package gr.aueb.cf;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git CF5");
+        System.out.println("Hello Git CF5 OPA");
     }
 
 }
